b13cc0f7d6e6c768606deef8bdac2a3c3ceb36f7
--unset <azurylldmm@gmail.com>
2024-07-24 16:35:26 -0400

    Initial commit.

---
+++ BETAS/Helpers/Log.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using StardewModdingAPI;
+using HarmonyLib;
+
+namespace BETAS.Helpers;
+
+public static class Log
+{
+    public static void Debug<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        LogLevel.Debug);
+
+    public static void Error<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        LogLevel.Error);
+
+    public static void Warn<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        LogLevel.Warn);
+
+    public static void Info<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        LogLevel.Info);
+
+    public static void Trace<T>(T message) => ModEntry.ModMonitor.Log(
+        $"{(message is not string ? "[" + message.GetType() + "] " : string.Empty)}{message.ToString() ?? string.Empty}",
+        LogLevel.Trace);
+
+    public static void ILCode(IEnumerable<CodeInstruction> code)
+    {
+        foreach (var instruction in code)
+        {
+            Debug($"{instruction.opcode} {instruction.operand}");
+        }
+    }
+
+    public static void ILCode(CodeInstruction code)
+    {
+        Debug($"{code.opcode} {code.operand}");
+    }
+}
---
+++ BETAS/ModEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using BETAS.Helpers;
+using StardewValley.Triggers;
+
+namespace BETAS
+{
+    internal sealed class ModEntry : Mod
+    {
+        internal static IMonitor ModMonitor { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+        
+        internal static IManifest Manifest { get; set; } = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            ModMonitor = Monitor;
+            Harmony = new Harmony(ModManifest.UniqueID);
+            Manifest = ModManifest;
+
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_LevelIncreased");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_ExperienceGained");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_LetterRead");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_CropHarvested");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_CropHarvestedByJunimo");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_CropDied");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_MachineOutputCollected");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_EnemyKilled");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_BedEntered");
+            TriggerActionManager.RegisterTrigger($"{Manifest.UniqueID}_SpouseKissed");
+
+            Harmony.PatchAll();
+
+            Helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+        }
+    }
+}
