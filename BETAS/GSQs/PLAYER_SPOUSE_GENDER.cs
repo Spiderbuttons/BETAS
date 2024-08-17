882d8668c1665a43151177a2aefb4cd0bd40b22b
--unset <azurylldmm@gmail.com>
2024-08-17 03:18:59 -0400

    Added PLAYER_SPOUSE_GENDER GSQ.

---
+++ BETAS/GSQs/PLAYER_SPOUSE_GENDER.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Minigames;
+
+namespace BETAS.GSQs;
+
+public static class PlayerSpouseGender
+{
+    // Checks whether a player's spouse is Male or Female.
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtility.TryGet(query, 1, out var playerKey, out var error) || !ArgUtility.TryGet(query, 2, out var genderName, out error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+        bool isMale = string.Equals(genderName, "Male", StringComparison.OrdinalIgnoreCase);
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, (Farmer target) =>
+        {
+            var spouse = target.getSpouse();
+            if (spouse == null)
+                return false;
+            return isMale == Utility.isMale(spouse.Name);
+        });
+    }
+}
