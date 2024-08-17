fb33cdc0172e367a53d3ca08279366df686fab13
--unset <azurylldmm@gmail.com>
2024-08-17 03:08:51 -0400

    Added PLAYER_STARDROPS_FOUND GSQ.

---
+++ BETAS/GSQs/PLAYER_STARDROPS_FOUND.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class PlayerStardropsFound
+{
+    // Checks how many stardrops a player has found. May or may not catch stardrops added through mods if the mod does not patch numStardropsFound.
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtility.TryGet(query, 1, out var playerKey, out var error) || !ArgUtility.TryGetFloat(query, 2, out var minDrops, out error) || !ArgUtility.TryGetOptionalFloat(query, 3, out var maxDrops, out error, float.MaxValue))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+        
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, delegate(Farmer target)
+        {
+            var num =  Utility.numStardropsFound(target);
+            return num >= minDrops && num <= maxDrops;
+        });
+    }
+}
