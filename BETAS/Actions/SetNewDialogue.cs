968097a627c77e9937e2a56edc3cf90abb460e0a
--unset <azurylldmm@gmail.com>
2024-08-24 06:12:09 -0400

    Updated Actions.md for new tokenizable strings for SetNewDialogue and TextAboveHead.

--- BETAS/Actions/SetNewDialogue.cs
+++ BETAS/Actions/SetNewDialogue.cs
@@ -26,14 +26,14 @@ public static class SetNewDialogue
             return false;
         }
 
-        try
-        {
-            dialogue = Game1.content.LoadString(dialogue);
-        }
-        catch
-        {
-            Log.Trace("BETAS.Actions.SetNewDialogue: Argument is not a translation key.");
-        }
+        // try
+        // {
+        //     dialogue = Game1.content.LoadString(dialogue);
+        // }
+        // catch
+        // {
+        //     Log.Trace("BETAS.Actions.SetNewDialogue: Argument is not a translation key.");
+        // }
 
         if (Game1.activeClickableMenu is not null && Game1.activeClickableMenu is DialogueBox dialogueBox &&
             dialogueBox.characterDialogue.speaker.Name.Equals(npcName))
