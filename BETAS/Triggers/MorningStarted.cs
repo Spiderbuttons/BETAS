f7c52122421cacec2042cccd499b0b8a5122a63b
--unset <azurylldmm@gmail.com>
2024-08-29 21:26:23 -0400

    Added MorningStarted trigger.

---
+++ BETAS/Triggers/MorningStarted.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Tools;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class MorningStarted
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Game1), nameof(Game1.doMorningStuff))]
+        public static void doMorningStuff_Postfix()
+        {
+            try
+            {
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_MorningStarted");
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.MorningStarted_Game1_doMorningStuff_Postfix: \n" + ex);
+            }
+        }
+    }
+}
