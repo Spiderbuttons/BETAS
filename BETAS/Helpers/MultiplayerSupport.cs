272b1357663a7f0989e7f2a95f9109482c4b8039
--unset <azurylldmm@gmail.com>
2024-09-24 15:22:45 -0400

    Enabled nullable types in MultiplayerSupport class.

--- BETAS/Helpers/MultiplayerSupport.cs
+++ BETAS/Helpers/MultiplayerSupport.cs
@@ -1,4 +1,5 @@
-﻿using StardewModdingAPI;
+﻿#nullable enable
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Mods;
@@ -12,10 +13,10 @@ public class MultiplayerSupport
     {
         public string TriggerName { get; set; }
         
-        public string TargetItemID { get; set; } 
+        public string? TargetItemID { get; set; } 
         public SerializableDictionary<string, string> TargetItemData { get; set; } = [];
         
-        public string InputItemID { get; set; }
+        public string? InputItemID { get; set; }
         public SerializableDictionary<string, string> InputItemData { get; set; } = [];
         
         public string Location { get; set; }
@@ -60,12 +61,11 @@ public class MultiplayerSupport
         if (e.FromModID != BETAS.Manifest.UniqueID || Context.IsMainPlayer || e.Type != "TriggerPackage") return;
 
         var triggerPackage = e.ReadAs<TriggerPackage>();
-        if (triggerPackage == null) return;
 
-        Item targetItem = triggerPackage.TargetItemID != null ? ItemRegistry.Create(triggerPackage.TargetItemID) : null;
-        Item inputItem = triggerPackage.InputItemID != null ? ItemRegistry.Create(triggerPackage.InputItemID) : null;
+        Item? targetItem = triggerPackage.TargetItemID != null ? ItemRegistry.Create(triggerPackage.TargetItemID) : null;
+        Item? inputItem = triggerPackage.InputItemID != null ? ItemRegistry.Create(triggerPackage.InputItemID) : null;
         Log.Debug("Got here");
-        GameLocation location = triggerPackage.Location != null ? Game1.getLocationFromName(triggerPackage.Location) : Game1.currentLocation;
+        GameLocation location = Game1.getLocationFromName(triggerPackage.Location) ?? Game1.currentLocation;
         Farmer player = triggerPackage.PlayerID != 0 ? Game1.getFarmer(triggerPackage.PlayerID) : Game1.player;
         
         if (targetItem != null)
