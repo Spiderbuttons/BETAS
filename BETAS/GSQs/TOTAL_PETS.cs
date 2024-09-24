bea22b4b810c76f4dda6cdaf37dfc45bd7a0c07c
--unset <azurylldmm@gmail.com>
2024-09-24 15:22:18 -0400

    Added TOTAL_PETS GSQ.

---
+++ BETAS/GSQs/TOTAL_PETS.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Characters;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class TotalPets
+{
+    // Checks how many pets there are on the farm.
+    [GSQ("TOTAL_PETS")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizableInt(query, 1, out var min, out var error) || 
+            !ArgUtilityExtensions.TryGetOptionalTokenizableInt(query, 2, out var max, out error, int.MaxValue) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizable(query, 3, out var _, out error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+        
+        if (max == -1) max = int.MaxValue;
+        var petCount = 0;
+
+        foreach (var farmer in Game1.getAllFarmers())
+        {
+            foreach (var character in Utility.getHomeOfFarmer(farmer).characters)
+            {
+                if (character is not Pet pet) continue;
+                if (!ArgUtility.HasIndex(query, 3))
+                {
+                    petCount++;
+                }
+                else if (GameStateQuery.Helpers.AnyArgMatches(query, 3, (petID) => pet.petType.Value.Equals(petID)))
+                {
+                    petCount++;
+                }
+            }
+        }
+
+        foreach (var character in Game1.getFarm().characters)
+        {
+            if (character is not Pet pet) continue;
+            if (!ArgUtility.HasIndex(query, 3))
+            {
+                petCount++;
+            }
+            else if (GameStateQuery.Helpers.AnyArgMatches(query, 3, (petID) => pet.petType.Value.Equals(petID)))
+            {
+                petCount++;
+            }
+        }
+        
+        Log.Alert(petCount);
+        
+        return petCount >= min && petCount <= max;
+    }
+}
