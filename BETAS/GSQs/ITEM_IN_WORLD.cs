89ffa69b182c212739a28963e1eefbd86d0863a0
--unset <azurylldmm@gmail.com>
2024-09-24 15:38:23 -0400

    Added ITEM_IN_WORLD GSQ.

---
+++ BETAS/GSQs/ITEM_IN_WORLD.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Characters;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class ItemInWorld
+{
+    // Checks if any given item ID exists somewhere in the world (in the inventory, chests, machines, etc)
+    [GSQ("ITEM_IN_WORLD")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var _, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        var found = false;
+        Utility.ForEachItem((item) =>
+        {
+            if (!GameStateQuery.Helpers.AnyArgMatches(query, 1,
+                    (itemID) => item.QualifiedItemId.Equals(ItemRegistry.QualifyItemId(itemID)))) return true;
+            found = true;
+            return false;
+        });
+        
+        return found;
+    }
+}
