3395a6735c2990985abf67fee8a5433796aeb9cf
--unset <azurylldmm@gmail.com>
2024-09-26 17:53:34 -0400

    Added WEDDING_TODAY GSQ.

---
+++ BETAS/GSQs/WEDDING_TODAY.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class WeddingToday
+{
+    // Check whether the volume of a specific category is set between a min and max value. Volume is a float between 0 and 1.
+    [GSQ("WEDDING_TODAY")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetOptionalTokenizable(query, 1, out var playerKey, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        if (!ArgUtility.HasIndex(query, 1)) return Game1.weddingToday;
+        
+        foreach (var wedding in Game1.weddingsToday) Log.Alert(wedding);
+        
+        return Game1.weddingToday && GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, (Farmer target) => target?.getSpouse() != null && target.GetSpouseFriendship().WeddingDate.Equals(Game1.Date));
+    }
+}
