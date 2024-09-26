b55019b14e9fcc81847e628500be9243dae45ce0
--unset <azurylldmm@gmail.com>
2024-09-26 17:58:52 -0400

    Added SALOON_DISH GSQ.

---
+++ BETAS/GSQs/SALOON_DISH.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class SaloonDish
+{
+    // Check whether the saloons dish of the day matches any given item ID.
+    [GSQ("SALOON_DISH")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var _, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.AnyArgMatches(query, 1, (dishName) => Game1.dishOfTheDay.QualifiedItemId.Equals(ItemRegistry.QualifyItemId(dishName), StringComparison.OrdinalIgnoreCase));
+    }
+}
