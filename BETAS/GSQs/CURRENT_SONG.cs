dde70ef9d8fb8bb6ac18e39b2aa89adb95907497
--unset <azurylldmm@gmail.com>
2024-09-26 17:53:24 -0400

    Added CURRENT_SONG GSQ.

---
+++ BETAS/GSQs/CURRENT_SONG.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class CurrentSong
+{
+    // Check whether the volume of a specific category is set between a min and max value. Volume is a float between 0 and 1.
+    [GSQ("CURRENT_SONG")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetOptionalTokenizable(query, 1, out var _, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        if (!ArgUtility.HasIndex(query, 1)) return (Game1.currentSong != null && Game1.currentSong.IsPlaying);
+        return Game1.currentSong != null && GameStateQuery.Helpers.AnyArgMatches(query, 1, (songName) => Game1.currentSong.Name.Equals(songName, StringComparison.OrdinalIgnoreCase));;
+    }
+}
