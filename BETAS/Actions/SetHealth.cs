754c3f75d28be7b9665f89d9757aa46d2bf0acc1
--unset <azurylldmm@gmail.com>
2024-09-27 00:58:34 -0400

    Added SetHealth and SetStamina actions.

---
+++ BETAS/Actions/SetHealth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.Actions;
+
+public static class SetHealth
+{
+    // Set the health of the current player to a given value.
+    [Action("SetHealth")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizableInt(args, 1, out var health, out error, allowBlank: false) || !ArgUtilityExtensions.TryGetOptionalTokenizableBool(args, 2, out var overrideBool, out error, defaultValue: false))
+        {
+            error = "Usage: SetHealth <Value> [Override Max]";
+            return false;
+        }
+        
+        Game1.player.health = overrideBool ? health : Math.Min(health, Game1.player.maxHealth);
+        return true;
+    }
+}
---
+++ BETAS/Actions/SetStamina.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.Actions;
+
+public static class SetStamina
+{
+    // Set the stamina of the current player to a given value.
+    [Action("SetStamina")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizableInt(args, 1, out var stamina, out error, allowBlank: false) || !ArgUtilityExtensions.TryGetOptionalTokenizableBool(args, 2, out var overrideBool, out error, defaultValue: false))
+        {
+            error = "Usage: SetStamina <Value> [Override Max]";
+            return false;
+        }
+        Game1.player.stamina = overrideBool ? stamina : Math.Min(stamina, Game1.player.MaxStamina);
+        return true;
+    }
+}
