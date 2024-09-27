385472bb7814b15f090b57f5caad0d1830405940
--unset <azurylldmm@gmail.com>
2024-09-27 00:24:52 -0400

    Added PLAYER_SWIMMING GSQ.

---
+++ BETAS/GSQs/PLAYER_SWIMMING.cs
@@ -0,0 +1,22 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class PlayerSwimming
+{
+    // Check whether the player is currently swimming.
+    [GSQ("PLAYER_SWIMMING")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var playerKey, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey,
+            target => target.swimming.Value);
+    }
+}
