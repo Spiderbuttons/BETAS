d1049a83e9ed881800925f7736c4367639c72f6d
--unset <azurylldmm@gmail.com>
2024-09-26 22:33:05 -0400

    Changed WEDDING_TODAY to require a player key and made the check a little less redundant.

--- BETAS/GSQs/WEDDING_TODAY.cs
+++ BETAS/GSQs/WEDDING_TODAY.cs
@@ -10,17 +10,15 @@ namespace BETAS.GSQs;
 
 public static class WeddingToday
 {
-    // Check whether or not there is a wedding scheduled today and optionally check which player is getting married.
+    // Check whether there is a wedding scheduled today and optionally check which player is getting married.
     [GSQ("WEDDING_TODAY")]
     public static bool Query(string[] query, GameStateQueryContext context)
     {
-        if (!ArgUtilityExtensions.TryGetOptionalTokenizable(query, 1, out var playerKey, out var error))
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var playerKey, out var error))
         {
             return GameStateQuery.Helpers.ErrorResult(query, error);
         }
-
-        if (!ArgUtility.HasIndex(query, 1)) return Game1.weddingToday;
         
-        return Game1.weddingToday && GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, (Farmer target) => target?.getSpouse() != null && target.GetSpouseFriendship().WeddingDate.Equals(Game1.Date));
+        return Game1.weddingToday && GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, (Farmer target) => target.GetSpouseFriendship() is not null && target.GetSpouseFriendship().WeddingDate.Equals(Game1.Date));
     }
 }
