dc69e823ebda01c354cd9b2d3c489cf0215be5c2
--unset <azurylldmm@gmail.com>
2024-09-28 00:27:23 -0400

    Added PLAYER_BACKPACK_SIZE GSQ.

---
+++ BETAS/GSQs/PLAYER_BACKPACK_SIZE.cs
@@ -0,0 +1,23 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class PlayerBackpackSize
+{
+    // Checks the number of backpack slots that a player has available to them
+    [GSQ("PLAYER_BACKPACK_SIZE")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var playerKey, out var error) ||
+            !ArgUtilityExtensions.TryGetTokenizableInt(query, 2, out var min, out error) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizableInt(query, 3, out var max, out error, int.MaxValue))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, target => target.MaxItems >= min && target.MaxItems <= max);
+    }
+}
