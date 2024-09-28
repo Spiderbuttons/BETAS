ef33e62aa21a21df313f5a7366e9bd6ffbc95080
--unset <azurylldmm@gmail.com>
2024-09-27 22:29:32 -0400

    Added MusicChanged trigger.

---
+++ BETAS/Triggers/MusicChanged.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class MusicChanged
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Game1), nameof(Game1.changeMusicTrack))]
+        public static void changeMusicTrack_Postfix(string newTrackName)
+        {
+            try
+            {
+                if (newTrackName is null || (Game1.IsGreenRainingHere() && !Game1.currentLocation.InIslandContext() && Game1.IsRainingHere(Game1.currentLocation) && !newTrackName.Equals("rain"))) return;
+                
+                var musicItem = ItemRegistry.Create(newTrackName);
+
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_MusicChanged", targetItem: musicItem);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.MusicChanged_Game1_changeMusicTrack_Postfix: \n" + ex);
+            }
+        }
+    }
+}
