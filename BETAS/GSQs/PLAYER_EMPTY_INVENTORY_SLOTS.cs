ba066800d918d69cee6e6a6d82cace9040702613
--unset <azurylldmm@gmail.com>
2024-09-28 00:27:35 -0400

    Added PLAYER_EMPTY_INVENTORY_SLOTS GSQ

---
+++ BETAS/GSQs/PLAYER_EMPTY_INVENTORY_SLOTS.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class PlayerEmptyInventorySlots
+{
+    // Checks the number of backpack slots that a player has available to them
+    [GSQ("PLAYER_EMPTY_INVENTORY_SLOTS")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var playerKey, out var error) ||
+            !ArgUtilityExtensions.TryGetTokenizableInt(query, 2, out var min, out error) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizableInt(query, 3, out var max, out error, int.MaxValue))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey, target => target.freeSpotsInInventory() >= min && target.freeSpotsInInventory() <= max);
+    }
+}
