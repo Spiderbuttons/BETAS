da5076baa06c2fbb3ab19dea06fba750d40890cd
--unset <azurylldmm@gmail.com>
2024-11-25 20:26:33 -0500

    Added Message action to display a plain dialogue box on the screen.

---
+++ BETAS/Actions/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.Actions;
+
+public static class Message
+{
+    // Make the current farmer perform an emote.
+    [Action("Message")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out var message, out error, allowBlank: true))
+        {
+            Log.Error(error);
+            return false;
+        }
+        if (!string.IsNullOrWhiteSpace(message))
+        {
+            Game1.drawDialogueNoTyping(message);
+            return true;
+        }
+
+        return false;
+    }
+}
