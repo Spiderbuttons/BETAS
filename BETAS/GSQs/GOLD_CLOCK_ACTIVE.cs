e3b7565e4a3ec139d1b48f9a1946ca018cf1c1f2
--unset <azurylldmm@gmail.com>
2024-12-11 20:12:22 -0500

    Added GOLD_CLOCK_ACTIVE GSQ.

---
+++ BETAS/GSQs/GOLD_CLOCK_ACTIVE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class GoldClockActive
+{
+    // Check whether the gold clock is a) built and b) active i.e. not turned off.
+    [GSQ("GOLD_CLOCK_ACTIVE")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        return Game1.IsBuildingConstructed("Gold Clock") && !Game1.netWorldState.Value.goldenClocksTurnedOff.Value;
+    }
+}
