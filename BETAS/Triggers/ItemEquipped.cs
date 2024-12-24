68a370903e64816654ddffd0bf6808736a2459e7
Spiderbuttons <azurylldmm@gmail.com>
2024-12-23 20:03:07 -0500

    Added ItemEquipped and ItemUnequipped triggers.

---
+++ BETAS/Triggers/ItemEquipped.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Tools;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class ItemEquipped
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Item), nameof(Item.onEquip))]
+        public static void onEquip_Postfix(Item __instance, Farmer who)
+        {
+            try
+            {
+                var itemItem = __instance.getOne();
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_ItemEquipped", targetItem: itemItem,
+                    location: who.currentLocation);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.ItemEquipped_Item_onEquip_Postfix: \n" + ex);
+            }
+        }
+    }
+}
---
+++ BETAS/Triggers/ItemUnequipped.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Tools;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class ItemUnequipped
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Item), nameof(Item.onUnequip))]
+        public static void onUnequip_Postfix(Item __instance, Farmer who)
+        {
+            try
+            {
+                var itemItem = __instance.getOne();
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_ItemUnequipped", targetItem: itemItem,
+                    location: who.currentLocation);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.ItemUnequipped_Item_onUnequip_Postfix: \n" + ex);
+            }
+        }
+    }
+}
