1014e28a84bbcdb48d545879a5e3e2f576bb2a42
Spiderbuttons <azurylldmm@gmail.com>
2025-01-02 22:53:11 -0500

    Added WEATHER_FOR_TOMORROW GSQ.

---
+++ BETAS/GSQs/WEATHER_TOMORROW.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Extensions;
+
+namespace BETAS.GSQs;
+
+public static class WeatherForTomorrow
+{
+    // Check whether the weather for tomorrow in a given location context is any of the given weather IDs.
+    [GSQ("WEATHER_FOR_TOMORROW")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var locationContext, out var error) || !ArgUtilityExtensions.TryGetTokenizable(query, 2, out var _, out error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        var weatherTomorrow = locationContext.EqualsIgnoreCase("Default") ? Game1.weatherForTomorrow : Game1.netWorldState.Value.GetWeatherForLocation(locationContext).WeatherForTomorrow;
+
+        return GameStateQuery.Helpers.AnyArgMatches(query, 2, (weather) => weatherTomorrow.EqualsIgnoreCase(weather));
+    }
+}
