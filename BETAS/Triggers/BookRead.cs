60378f08893f32217357490c24c4d2075e5511e7
Spiderbuttons <azurylldmm@gmail.com>
2025-01-14 04:10:27 -0500

    Added BookRead trigger.

---
+++ BETAS/Triggers/BookRead.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.GameData.Minecarts;
+using StardewValley.Triggers;
+using Object = StardewValley.Object;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class BookRead
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Object), "readBook")]
+        public static void readBook_Postfix(Object __instance, GameLocation location)
+        {
+            try
+            {
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_BookRead",
+                    location: location, targetItem: ItemRegistry.Create(__instance.QualifiedItemId));
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.BookRead_Object_readBook_Postfix: \n" + ex);
+            }
+        }
+    }
+}
