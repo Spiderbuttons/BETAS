a126b9f9fdae2c92c56bf93ba80bc50a7116e51c
Spiderbuttons <azurylldmm@gmail.com>
2025-01-19 20:29:38 -0500

    Added ShopOpened trigger.

---
+++ BETAS/Triggers/ShopOpened.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class ShopOpened
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            return typeof(ShopMenu).GetConstructors();
+        }
+        
+        public static void Postfix(ShopMenu __instance)
+        {
+            try
+            {
+                var shopItem = ItemRegistry.Create(__instance.ShopId);
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_ShopOpened", targetItem: shopItem);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.ShopOpened_ShopMenu_ctors_Postfix: \n" + ex);
+            }
+        }
+    }
+}
