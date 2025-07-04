be32c90fde90324d08f692ffb1e5400de65f2b88
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 18:28:16 -0400

    Added MailReceived and MailRemoved triggers.

---
+++ BETAS/Triggers/MailReceived.cs
@@ -0,0 +1,32 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using Netcode;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Quests;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class MailReceived
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        public static void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Game1.player.mailReceived.OnValueAdded += OnMailReceived;
+        }
+
+        public static void OnMailReceived(string value)
+        {
+            var mailItem = ItemRegistry.Create(value);
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_MailReceived", inputItem: mailItem, targetItem: mailItem);
+        }
+    }
+}
---
+++ BETAS/Triggers/MailRemoved.cs
@@ -0,0 +1,32 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using Netcode;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Quests;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class MailRemoved
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        public static void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Game1.player.mailReceived.OnValueRemoved += OnMailRemoved;
+        }
+
+        public static void OnMailRemoved(string value)
+        {
+            var mailItem = ItemRegistry.Create(value);
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_MailRemoved", inputItem: mailItem, targetItem: mailItem);
+        }
+    }
+}
