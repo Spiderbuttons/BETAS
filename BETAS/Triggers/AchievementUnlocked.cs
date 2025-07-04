3a06c042c12eeef3fae29222a33acac987eb6898
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 18:48:46 -0400

    Added AchievementUnlocked trigger.

---
+++ BETAS/Triggers/AchievementUnlocked.cs
@@ -0,0 +1,31 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using Netcode;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Quests;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class AchievementUnlocked
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        public static void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Game1.player.achievements.OnValueAdded += OnAchievementUnlocked;
+        }
+
+        public static void OnAchievementUnlocked(int value)
+        {
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_AchievementUnlocked");
+        }
+    }
+}
