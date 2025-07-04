2383b4114f19fb08545baa1eb6cea6785ebef02e
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 16:06:45 -0400

    Added ActionList action.

---
+++ BETAS/TriggerActions/ActionList.cs
@@ -0,0 +1,30 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley.Delegates;
+using StardewValley.Triggers;
+
+namespace BETAS.TriggerActions;
+
+public static class ActionList
+{
+    // Run every action in an action list, one after the other.
+    [Action("ActionList")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out var _, out error, allowBlank: false))
+        {
+            error = "Usage: Spiderbuttons.BETAS_ActionList <Action String>+";
+            return false;
+        }
+
+        for (var i = 1; i < args.Length; i++)
+        {
+            if (!TriggerActionManager.TryRunAction(args[i], out error, out _))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
