f8fd951ee53f09b5c4988728be6de8850565b34a
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:11:04 -0400

    Added GenderChanged/TransGendered trigger.

---
+++ BETAS/Triggers/GenderChanged.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Network;
+using StardewValley.Tools;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class GenderChanged
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Farmer), nameof(Farmer.changeGender))]
+        public static void changeGender_Postfix()
+        {
+            try
+            {
+                TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_GenderChanged");
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.GenderChanged_Farmer_changeGender_Postfix: \n" + ex);
+            }
+        }
+    }
+}
