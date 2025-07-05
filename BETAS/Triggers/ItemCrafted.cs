d9bccde53a6af7d2ab0ef397743288607df6f088
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:10:42 -0400

    Added ItemCrafted trigger.

---
+++ BETAS/Triggers/ItemCrafted.cs
@@ -0,0 +1,32 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using Netcode;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Quests;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class ItemCrafted
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        public static void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Game1.player.craftingRecipes.OnValueAdded += OnItemCrafted;
+        }
+
+        public static void OnItemCrafted(string itemId, int count)
+        {
+            var item = ItemRegistry.Create(itemId);
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_ItemCrafted", inputItem: item, targetItem: item);
+        }
+    }
+}
