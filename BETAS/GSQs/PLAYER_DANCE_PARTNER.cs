39458d61f5d50df1a1ebdd2924757da70d26cb6a
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:11:25 -0400

    Added PLAYER_DANCE_PARTNER GSQ.

---
+++ BETAS/GSQs/PLAYER_DANCE_PARTNER.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Extensions;
+
+namespace BETAS.GSQs;
+
+public static class PlayerDancePartner
+{
+    // Check whether the player has a dance partner, and optionally whether it matches a given NPC name.
+    [GSQ("PLAYER_DANCE_PARTNER")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetOptionalTokenizable(query, 1, out var npc, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        var partner = Game1.player.dancePartner.GetCharacter();
+        
+        if (partner == null) return npc == null;
+        if (npc is null or "Any") return true;   
+        if (npc.EqualsIgnoreCase("Farmer")) return partner is Farmer;
+        return partner.Name.EqualsIgnoreCase(npc);
+    }
+}
