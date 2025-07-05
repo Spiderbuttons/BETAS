e8b352a2f2beb09b637244c298346e377a2a11c8
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:12:14 -0400

    Added PLAYER_FESTIVAL_SCORE GSQ.

---
+++ BETAS/GSQs/PLAYER_FESTIVAL_SCORE.cs
@@ -0,0 +1,24 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class PlayerFestivalScore
+{
+    // Check whether the players current festival score is within a given range.
+    [GSQ("PLAYER_FESTIVAL_SCORE")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var playerKey, out var error) ||
+            !ArgUtilityExtensions.TryGetTokenizableInt(query, 2, out var min, out error) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizableInt(query, 3, out var max, out error, int.MaxValue))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey,
+            target => target.festivalScore >= min && target.festivalScore <= max);
+    }
+}
