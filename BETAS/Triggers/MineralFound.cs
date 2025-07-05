d51953022b12a1ba0bc25366eca762caf5d44b80
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:10:20 -0400

    Added MineralFound trigger.

---
+++ BETAS/Triggers/MineralFound.cs
@@ -0,0 +1,32 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using Netcode;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Quests;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class MineralFound
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        public static void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Game1.player.mineralsFound.OnValueAdded += OnMineralFound;
+        }
+
+        public static void OnMineralFound(string mineral, int count)
+        {
+            var item = ItemRegistry.Create(mineral);
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_MineralFound", inputItem: item, targetItem: item);
+        }
+    }
+}
