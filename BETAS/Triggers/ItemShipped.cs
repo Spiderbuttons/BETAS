885b90242d88aa8569caf83bb149a895daa6e61e
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:10:31 -0400

    Added ItemShipped trigger.

---
+++ BETAS/Triggers/ItemShipped.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Menus;
+using StardewValley.Tools;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class ItemShipped
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(ShippingMenu), nameof(ShippingMenu.parseItems))]
+        public static void parseItems_Postfix(IList<Item> items)
+        {
+            try
+            {
+                foreach (var item in items)
+                {
+                    var itemCopy = item.getOne();
+                    itemCopy.Stack = item.Stack;
+                    Log.Alert($"ItemShipped: {itemCopy.DisplayName} ({itemCopy.Stack})");
+                    TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_ItemShipped", inputItem: itemCopy, targetItem: itemCopy);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error in BETAS.ItemShipped_ShippingMenu_parseItems_Postfix: \n" + ex);
+            }
+        }
+    }
+}
