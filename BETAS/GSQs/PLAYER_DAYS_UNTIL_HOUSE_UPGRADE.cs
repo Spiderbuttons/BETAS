ddcbac71b2dbe9b5c537489d0caf314c2990fe78
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:11:38 -0400

    Added PLAYER_DAYS_UNTIL_HOUSE_UPGRADE GSQ.

---
+++ BETAS/GSQs/PLAYER_DAYS_UNTIL_HOUSE_UPGRADE.cs
@@ -0,0 +1,24 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class PlayerDaysUntilHouseUpgrade
+{
+    // Check whether the player has a house upgrade pending and/or how long until it's done.
+    [GSQ("PLAYER_DAYS_UNTIL_HOUSE_UPGRADE")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var playerKey, out var error) ||
+            !ArgUtilityExtensions.TryGetTokenizableInt(query, 2, out var minDays, out error) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizableInt(query, 3, out var maxDays, out error, int.MaxValue))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.WithPlayer(context.Player, playerKey,
+            target => target.daysUntilHouseUpgrade.Value >= minDays && target.daysUntilHouseUpgrade.Value <= maxDays);
+    }
+}
