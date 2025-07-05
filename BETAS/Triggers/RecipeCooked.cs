5533919481ba7fbc381e608810d31b502913b45f
Spiderbuttons <azurylldmm@gmail.com>
2025-07-04 21:10:51 -0400

    Added RecipeCooked trigger.

---
+++ BETAS/Triggers/RecipeCooked.cs
@@ -0,0 +1,32 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using HarmonyLib;
+using Netcode;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Quests;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class RecipeCooked
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        public static void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            Game1.player.recipesCooked.OnValueAdded += OnRecipeCooked;
+        }
+
+        public static void OnRecipeCooked(string itemId, int count)
+        {
+            var item = ItemRegistry.Create(itemId);
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_RecipeCooked", inputItem: item, targetItem: item);
+        }
+    }
+}
