5b89292ae38a3ab0a656ad97bec432d60a1cbebc
Spiderbuttons <azurylldmm@gmail.com>
2025-07-10 19:34:55 -0400

    Added NPC_KISSED_TODAY GSQ.

---
+++ BETAS/GSQs/NPC_KISSED_TODAY.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.GSQs;
+
+public static class NpcKissedToday
+{
+    // Check whether a given NPC has been kissed today by any farmer.
+    [GSQ("NPC_KISSED_TODAY")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var _, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+
+        return GameStateQuery.Helpers.AnyArgMatches(query, 1, (name) =>
+            {
+                var npc = Game1.getCharacterFromName(name);
+                if (npc != null) return npc.hasBeenKissedToday.Value;
+                
+                Log.Warn($"No NPC found with name '{name}'");
+                return false;
+            });
+    }
+}
