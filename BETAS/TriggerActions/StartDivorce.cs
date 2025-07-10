0466fd8bbc17d79818c72285537cccb3d0033fc6
Spiderbuttons <azurylldmm@gmail.com>
2025-07-10 19:24:24 -0400

    Added StartDivorce action.

---
+++ BETAS/TriggerActions/StartDivorce.cs
@@ -0,0 +1,18 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class StartDivorce
+{
+    // Set the player to be divorced tonight.
+    [Action("StartDivorce")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        error = null;
+        Game1.player.divorceTonight.Value = true;
+        return true;
+    }
+}
