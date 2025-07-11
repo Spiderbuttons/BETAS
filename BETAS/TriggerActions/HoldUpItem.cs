811a5f7aff19130a6fc92f05846f10ed28465f28
Spiderbuttons <azurylldmm@gmail.com>
2025-07-10 20:15:01 -0400

    Added HoldUpItem action.

---
+++ BETAS/TriggerActions/HoldUpItem.cs
@@ -0,0 +1,24 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class HoldUpItem
+{
+    // Give the player an item and make them hold it up.
+    [Action("HoldUpItem")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out var itemId, out error) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizableInt(args, 2, out var count, out error, defaultValue: 1))
+        {
+            error = "Usage: Spiderbuttons.BETAS_HoldUpItem <ItemId> [Count]";
+            return false;
+        }
+
+        Game1.PerformActionWhenPlayerFree(() => Game1.player.holdUpItemThenMessage(ItemRegistry.Create(itemId), count));
+        return true;
+    }
+}
