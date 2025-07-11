1dee091299aafc59f418abff5fc22ee8de433597
Spiderbuttons <azurylldmm@gmail.com>
2025-07-10 20:16:12 -0400

    Added WriteFarmModData and RemoveFarmModData actions.

---
+++ BETAS/TriggerActions/RemoveFarmModData.cs
@@ -0,0 +1,33 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class RemoveFarmModData
+{
+    // Remove an entry from the current farm's mod data dictionary, optionally only if the current value matches a given value.
+    [Action("RemoveFarmModData")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out string key, out error) || !ArgUtilityExtensions.TryGetOptionalTokenizable(args, 2, out string value, out error))
+        {
+            error = "Usage: Spiderbuttons.BETAS_RemoveFarmModData <Key> [Value]";
+            return false;
+        }
+
+        if (!ArgUtility.HasIndex(args, 2))
+        {
+            Game1.getFarm().modData.Remove(key);
+        }
+        else
+        {
+            if (Game1.getFarm().modData.TryGetValue(key, out var existingValue) && existingValue == value)
+            {
+                Game1.getFarm().modData.Remove(key);
+            }
+        }
+        return true;
+    }
+}
---
+++ BETAS/TriggerActions/WriteFarmModData.cs
@@ -0,0 +1,23 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class WriteFarmModData
+{
+    // Write a value to the current farm's mod data with a given key.
+    [Action("WriteFarmModData")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out string key, out error) || !ArgUtilityExtensions.TryGetTokenizable(args, 2, out string value, out error))
+        {
+            error = "Usage: Spiderbuttons.BETAS_WriteFarmModData <Key> <Value>";
+            return false;
+        }
+
+        Game1.getFarm().modData[key] = value;
+        return true;
+    }
+}
