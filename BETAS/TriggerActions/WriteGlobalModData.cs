1053542b91f72784adea57396f0ba30130cca838
Spiderbuttons <azurylldmm@gmail.com>
2025-07-12 00:42:26 -0400

    Added WriteGlobalModData action.

---
+++ BETAS/TriggerActions/WriteGlobalModData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BETAS.AdvancedPermissions;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewModdingAPI;
+using StardewModdingAPI.Framework.ModHelpers;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class WriteGlobalModData
+{
+    // Write a value to global mod data with a given key.
+    [Action("WriteGlobalModData")]
+    public static bool Action(string[] args, TriggerActionContext context, out string error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out string uniqueId, out error) ||
+            !ArgUtilityExtensions.TryGetTokenizable(args, 2, out string key, out error) ||
+            !ArgUtilityExtensions.TryGetTokenizable(args, 3, out string value, out error))
+        {
+            error = "Usage: Spiderbuttons.BETAS_WriteGlobalModData <UniqueId> <Key> <Value>";
+            return false;
+        }
+
+        var mod = BETAS.ModRegistry.Get(uniqueId);
+        if (mod == null)
+        {
+            error = $"No mod found with unique ID '{uniqueId}'.";
+            return false;
+        }
+
+        return GlobalModData.TryWriteGlobalModData(mod, key, value, out error);
+    }
+}
