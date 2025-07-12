a576587cc00cc98f53a93751bcdf9827a3b49afa
Spiderbuttons <azurylldmm@gmail.com>
2025-07-12 00:41:23 -0400

    Added FarmModData TKString.

---
+++ BETAS/TokenizableStrings/FarmModData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.TokenizableStrings;
+
+namespace BETAS.TokenizableStrings;
+
+public static class TKFarmModData
+{
+    /// <summary>When given a key, returns the value of that key in the farm's mod data.</summary>
+    /// <inheritdoc cref="T:StardewValley.TokenizableStrings.TokenParserDelegate" />
+    [TKString("FarmModData")]
+    public static bool Parse(string[] query, out string replacement, Random random, Farmer player)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var key, out var error))
+        {
+            return TokenParser.LogTokenError(query, error, out replacement);
+        }
+        
+        if (!Game1.getFarm().modData.TryGetValue(key, out var value))
+        {
+            return TokenParser.LogTokenError(query, $"Key not found in farm mod data: {key}", out replacement);
+        }
+        
+        replacement = value;
+        return true;
+    }
+}
