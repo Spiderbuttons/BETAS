bbc13ae067d968375d351f15ddd198fc922a98a3
Spiderbuttons <azurylldmm@gmail.com>
2025-07-14 02:00:41 -0400

    Added PlaySound action.

---
+++ BETAS/TriggerActions/PlaySound.cs
@@ -0,0 +1,26 @@
+﻿using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class PlaySound
+{
+    // Play a sound.
+    [Action("PlaySound")]
+    public static bool Action(string[] args, TriggerActionContext context, out string? error)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out var cueName, out error, allowBlank: true) ||
+            !ArgUtilityExtensions.TryGetOptionalTokenizableBool(args, 2, out var local, out error, defaultValue: false))
+        {
+            error = "Usage: Spiderbuttons.BETAS_PlaySound <Sound> [Everyone?]";
+            return false;
+        }
+
+        if (local) Game1.player.playNearbySoundLocal(cueName);
+        else Game1.player.playNearbySoundAll(cueName);
+
+        return true;
+    }
+}
