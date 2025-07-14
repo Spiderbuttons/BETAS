5469272c949c6875bae8c2e59619992391c950bf
Spiderbuttons <azurylldmm@gmail.com>
2025-07-14 17:22:46 -0400

    Tweaked how OpenShop works a bit.

--- BETAS/TriggerActions/OpenShop.cs
+++ BETAS/TriggerActions/OpenShop.cs
@@ -12,16 +12,13 @@ public static class OpenShop
     public static bool Action(string[] args, TriggerActionContext context, out string? error)
     {
         if (!ArgUtilityExtensions.TryGetTokenizable(args, 1, out string? shopId, out error) ||
-            !ArgUtilityExtensions.TryGetOptionalTokenizable(args, 2, out string? ownerName, out error))
+            !ArgUtilityExtensions.TryGetOptionalTokenizable(args, 2, out string? ownerName, out error, defaultValue: null))
         {
             error = "Usage: Spiderbuttons.BETAS_OpenShop <ShopId> [Owner]";
             return false;
         }
-        
-        if (ownerName != null ? Utility.TryOpenShopMenu(shopId, ownerName) : Utility.TryOpenShopMenu(shopId, Game1.player.currentLocation, forceOpen: true))
-        {
-            return true;
-        }
+
+        if (Utility.TryOpenShopMenu(shopId, ownerName)) return true;
         
         error = $"Failed to open shop with ID '{shopId}'" + (ownerName != null ? $" for owner '{ownerName}'" : string.Empty);
         return false;
