3bf8cfef750b4f113cd99529e9c5568f450e7e43
Spiderbuttons <azurylldmm@gmail.com>
2025-07-30 21:14:20 -0400

    Added GIANT_CROP_GROWN GSQ.

---
+++ BETAS/GSQs/GIANT_CROP_GROWN.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Extensions;
+using StardewValley.TerrainFeatures;
+using StardewValley.TokenizableStrings;
+
+namespace BETAS.GSQs;
+
+public static class GiantCropGrown
+{
+    // Check whether any of the given giant crops have been grown and currently exist in the world.
+    [GSQ("GIANT_CROP_GROWN")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!ArgUtilityExtensions.TryGetTokenizable(query, 1, out var cropId, out var error))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+        
+        // Might as well search the farm first, since it's the most likely place to find giant crops.
+        if (Game1.getFarm().resourceClumps.Any(clump => clump is GiantCrop crop && crop.Id.EqualsIgnoreCase(cropId)))
+        {
+            return true;
+        }
+
+        bool found = false;
+        Utility.ForEachLocation(location =>
+        {
+            if (location is Farm) return true;
+            if (location.resourceClumps.Any(clump => clump is GiantCrop crop && crop.Id.EqualsIgnoreCase(cropId)))
+            {
+                found = true;
+                return false;
+            }
+
+            return true;
+        });
+
+        return found;
+    }
+}
