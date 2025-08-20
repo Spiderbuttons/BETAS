6b077a746e01773648ebaa6f1c9f7de311326f7a
Spiderbuttons <azurylldmm@gmail.com>
2025-08-19 21:06:17 -0400

    Added CHECK_AND_SET/REMOVE_MAIL GSQs.

---
+++ BETAS/GSQs/CHECK_AND_REMOVE_MAIL.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Network.NetEvents;
+
+namespace BETAS.GSQs;
+
+public static class CheckAndRemoveMail
+{
+    // Check whether the current player has a specific mail flag and set it after the check.
+    [GSQ("CHECK_AND_REMOVE_MAIL")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!TokenizableArgUtility.TryGet(query, 1, out var flag, out var error, name: "string Mail ID") ||
+            !TokenizableArgUtility.TryGetOptional(query, 2, out var mailbox, out error, defaultValue: "any", allowBlank: true, name: "string Mailbox Type"))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+        
+        bool hasFlag;
+
+        switch (mailbox.ToLower())
+        {
+            case "mailbox":
+                hasFlag = Game1.player.mailbox.Contains(flag);
+                Game1.player.mailbox.Remove(flag);
+                break;
+            case "tomorrow":
+                hasFlag = Game1.player.mailForTomorrow.Contains(flag);
+                Game1.player.mailForTomorrow.Remove(flag);
+                break;
+            case "received":
+                hasFlag = Game1.player.mailReceived.Contains(flag);
+                Game1.player.mailReceived.Remove(flag);
+                break;
+            case "any":
+                hasFlag = Game1.player.mailbox.Contains(flag) ||
+                          Game1.player.mailForTomorrow.Contains(flag) ||
+                          Game1.player.mailReceived.Contains(flag);
+                Game1.player.mailbox.Remove(flag);
+                Game1.player.mailForTomorrow.Remove(flag);
+                Game1.player.mailReceived.Remove(flag);
+                break;
+            default:
+                return GameStateQuery.Helpers.ErrorResult(query, "unknown mailbox type '" + mailbox + "'; expected 'Mailbox', 'Tomorrow', 'Received', or 'Any'");
+        }
+
+        return hasFlag;
+    }
+}
---
+++ BETAS/GSQs/CHECK_AND_SET_MAIL.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Network.NetEvents;
+
+namespace BETAS.GSQs;
+
+public static class CheckAndSetMail
+{
+    // Check whether the current player has a specific mail flag and set it after the check.
+    [GSQ("CHECK_AND_SET_MAIL")]
+    public static bool Query(string[] query, GameStateQueryContext context)
+    {
+        if (!TokenizableArgUtility.TryGet(query, 1, out var flag, out var error, name: "string Mail ID") ||
+            !TokenizableArgUtility.TryGetOptional(query, 2, out var mailbox, out error, defaultValue: "any", allowBlank: true, name: "string Mailbox Type"))
+        {
+            return GameStateQuery.Helpers.ErrorResult(query, error);
+        }
+        
+        bool hasFlag;
+
+        switch (mailbox.ToLower())
+        {
+            case "mailbox":
+                hasFlag = Game1.player.mailbox.Contains(flag);
+                Game1.player.mailbox.Add(flag);
+                break;
+            case "tomorrow":
+                hasFlag = Game1.player.mailForTomorrow.Contains(flag);
+                Game1.player.mailForTomorrow.Add(flag);
+                break;
+            case "received":
+                hasFlag = Game1.player.mailReceived.Contains(flag);
+                Game1.player.mailReceived.Add(flag);
+                break;
+            case "any":
+                hasFlag = Game1.player.mailbox.Contains(flag) ||
+                          Game1.player.mailForTomorrow.Contains(flag) ||
+                          Game1.player.mailReceived.Contains(flag);
+                Game1.player.mailbox.Add(flag);
+                Game1.player.mailForTomorrow.Add(flag);
+                Game1.player.mailReceived.Add(flag);
+                break;
+            default:
+                return GameStateQuery.Helpers.ErrorResult(query, "unknown mail type '" + mailbox + "'; expected 'Mailbox', 'Tomorrow', 'Received', or 'Any'");
+        }
+
+        return hasFlag;
+    }
+}
