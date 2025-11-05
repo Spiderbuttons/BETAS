0991ab9a639a50a11917507ee3e1a46ae3d81c27
Spiderbuttons <azurylldmm@gmail.com>
2025-11-05 13:14:20 -0500

    Added SaveLoaded trigger.

---
+++ BETAS/Triggers/SaveLoaded.cs
@@ -0,0 +1,22 @@
+﻿using BETAS.Attributes;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    static class SaveLoaded
+    {
+        public static void Initialize()
+        {
+            BETAS.ModHelper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+        }
+        
+        private static void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            var saveItem = ItemRegistry.Create(Game1.GetSaveGameName() ?? "SaveLoaded");
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_SaveLoaded", targetItem: saveItem);
+        }
+    }
+}
