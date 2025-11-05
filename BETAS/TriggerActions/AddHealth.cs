c563fede9e44aabcae5c34ddf80392b4b42b0dec
Spiderbuttons <azurylldmm@gmail.com>
2025-11-05 13:42:53 -0500

    Added AddHealth and AddStamina tractions.

---
+++ BETAS/TriggerActions/AddHealth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class AddHealth
+{
+    // Add a given value to the health of the current player.
+    [Action("AddHealth")]
+    public static bool Action(string[] args, TriggerActionContext context, out string? error)
+    {
+        if (!TokenizableArgUtility.TryGetInt(args, 1, out var health, out error, name: "int #Health") ||
+            !TokenizableArgUtility.TryGetOptionalBool(args, 2, out var overrideBool, out error, defaultValue: false, name: "bool Override Max?"))
+        {
+            return false;
+        }
+
+        Game1.player.health += health;
+        if (!overrideBool) Game1.player.health = Math.Min(Game1.player.health, Game1.player.maxHealth);
+        return true;
+    }
+}
---
+++ BETAS/TriggerActions/AddStamina.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BETAS.Attributes;
+using BETAS.Helpers;
+using StardewValley;
+using StardewValley.Delegates;
+
+namespace BETAS.TriggerActions;
+
+public static class AddStamina
+{
+    // Add a given value to the health of the current player.
+    [Action("AddStamina")]
+    public static bool Action(string[] args, TriggerActionContext context, out string? error)
+    {
+        if (!TokenizableArgUtility.TryGetInt(args, 1, out var stamina, out error, name: "int #Stamina") ||
+            !TokenizableArgUtility.TryGetOptionalBool(args, 2, out var overrideBool, out error, defaultValue: false, name: "bool Override Max?"))
+        {
+            return false;
+        }
+
+        Game1.player.stamina += stamina;
+        if (!overrideBool) Game1.player.stamina = Math.Min(Game1.player.stamina, Game1.player.MaxStamina);
+        return true;
+    }
+}
