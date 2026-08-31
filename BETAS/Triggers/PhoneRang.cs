331dd777d33049a727bebb6a437e52c3025fb8e8
Spiderbuttons <azurylldmm@gmail.com>
2026-08-30 22:12:44 -0400

    Added PhoneRang trigger.

---
+++ BETAS/Triggers/PhoneRang.cs
@@ -0,0 +1,23 @@
+﻿using BETAS.Attributes;
+using HarmonyLib;
+using StardewValley;
+using StardewValley.Objects;
+using StardewValley.Triggers;
+
+namespace BETAS.Triggers
+{
+    [Trigger]
+    [HarmonyPatch]
+    static class PhoneRang
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Phone), nameof(Phone.Ring))]
+        public static void Ring_Postfix(Phone __instance, string callId)
+        {
+            if (string.IsNullOrWhiteSpace(callId)) return; // Means it actually STOPPED ringing.
+
+            Item ringItem = ItemRegistry.Create(callId);
+            TriggerActionManager.Raise($"{BETAS.Manifest.UniqueID}_PhoneRang", targetItem: ringItem, inputItem: __instance, location: __instance.Location);
+        }
+    }
+}
